a84be08bb549d064225bda437fcf9ae8d428065a
haha1250 <dinhvietha1212@gmail.com>
2025-07-03 00:17:01 +0200

    Fix tailwind dependencies

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,8 +1,9 @@
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
 export default {
   plugins: [
-    require('@tailwindcss/postcss')({
-      config: './tailwind.config.js', // optional path
-    }),
-    require('autoprefixer'),
+    tailwindcss({ config: './tailwind.config.js' }),
+    autoprefixer,
   ],
 };
